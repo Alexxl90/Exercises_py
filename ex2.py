fb926bb6d6c58a1db5d4f1e208559b07ec5d5aac
Alex <hirbu.alexandru@gmail.com>
2023-01-09 20:25:37 +0200

    Test

--- ex2.py
+++ ex2.py
@@ -7,5 +7,5 @@ for element in a:
     if element in b:
         c.append(element)
 c = set(c) #initial nu am facut chestia asta si imi afisa si duplicate. Am inteles destul de bine ce face si ce este "set"
-#c = list(set(c))->am gasit si solutia asta pe net dar nu inteleg de ce as schimba din set in lista atat timp cat imi intoarce acelasi rezultat.
+#c = list(set(c))->am gasit si solutia asta pe    net dar nu inteleg de ce as schimba din set in lista atat timp cat imi intoarce acelasi rezultat.
 print (sorted(c))
