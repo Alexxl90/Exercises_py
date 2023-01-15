70ddbde2429a7a8da9a4a564a1f07aaf3b1b27d3
Alex <hirbu.alexandru@gmail.com>
2023-01-15 23:03:14 +0200

    added automatic metric converter

--- personal_data _sheet.py
+++ personal_data _sheet.py
@@ -2,13 +2,21 @@ First_name = input('Enter your first name: \n' )
 Last_name = input('Enter your last name: \n')
 message = f'Hi {First_name.title()} {Last_name.title()} I have another question for you:' 
 print (message)
-weight_in_pounds = input ('Tell me your exact weight in pounds \n')
 
-weight_in_kg = 0.4536 * float(weight_in_pounds)
-print ("I've calculate your weight in kg -> " , weight_in_kg , 'kg')
+weight = input ('Tell me your exact weight  \n')
+print ("(L)bs or (K)g")
+metric = input()
+if metric == "K" or metric == "k":
+    mess =  2.205 * float(weight)
+    print ('You are', mess ,'lbs')
+elif metric == "L" or metric == "l":
+    mess_2 =   0.4536 * float(weight) 
+    print ('You are' , mess_2 , 'kg')
+
+
 nickname = f'{First_name[0]}{Last_name[0:1]}'
 nickname_1 = f'{Last_name[2]}'
-print ('Here is you alias on this site: ', nickname.upper() + nickname_1.lower()) 
+print ('Here is you alias for this site: ', nickname.upper() + nickname_1.lower()) 
 print ('Have a nice day!')
 
 
