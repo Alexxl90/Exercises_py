9b41b1bc0bb17c9ee962f39a1b7d39f736ee49e3
Alex <hirbu.alexandru@gmail.com>
2023-01-15 14:52:33 +0200

    Good bye message added

--- personal_data _sheet.py
+++ personal_data _sheet.py
@@ -3,10 +3,13 @@ Last_name = input('Enter your last name: \n')
 message = f'Hi {First_name.title()} {Last_name.title()} I have another question for you:' 
 print (message)
 weight_in_pounds = input ('Tell me your exact weight in pounds \n')
+
 weight_in_kg = 0.4536 * float(weight_in_pounds)
 print ("I've calculate your weight in kg -> " , weight_in_kg , 'kg')
 nickname = f'{First_name[0]}{Last_name[0:1]}'
 nickname_1 = f'{Last_name[2]}'
-print ( nickname.upper() + nickname_1.lower()) 
+print ('Here is you alias on this site: ', nickname.upper() + nickname_1.lower()) 
+print ('Have a nice day!')
+
 
 
