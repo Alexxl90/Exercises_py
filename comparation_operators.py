6e8a07aed9805fe78e0f88bce4fa769e947f228e
Alex <hirbu.alexandru@gmail.com>
2023-01-15 22:07:51 +0200

    first part removed. Added solving for the exercise

--- comparation_operators.py
+++ comparation_operators.py
@@ -1,17 +1,13 @@
-# if temperature is greater than 30 itșs a hot day, 
-# otherwise if it's less than 10 it's a cold day, otherwise it's neither hot or cold
-
-
-temprature = int(input())
-if temprature > 30:
-    print ("It's a hot day")
-elif temprature < 10 :
-    print ("It's a cold day")
-elif  temprature == 24 :
-    print ('Perfect temp')
-else:
-    print ('Normal temp')    
 
 #if name is less than 3 characters long display the error "Name must be at least 3 characters long"
-#otherwise if it's more than 50 characters long  display the error "Nama must be  a max of 25 characters" 
+#otherwise if it's more than 25 characters long  display the error "Name must be  a max of 25 characters" 
 # otherwise display "Name loks good"
+print ("Enter your name: ")
+name = input()
+name_characters_count =  len(name)
+if name_characters_count <= 3:
+    print ("Name must be at least 3 characters long")
+elif name_characters_count >= 25:
+    print ("Name must be  a max of 25 characters")
+else:
+    print("Name loks good")
