3b7d004f1e786dc8bd959e7aea5a748eb41f5eaf
Alex <hirbu.alexandru@gmail.com>
2023-01-15 16:29:28 +0200

    Mosh_ex

---
+++ Logical operators.py
@@ -0,0 +1,11 @@
+# If applicant has hight income , good credit and no criminal record , is eligible for loan
+
+
+high_income = True
+good_credit = False
+criminal_record = False
+if high_income or good_credit or not criminal_record:
+    print ('eligible ')
+else:
+    print ('not eligible')
+
