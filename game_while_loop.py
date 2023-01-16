eb6835735ecc32986f380b56e24755e197481d6c
Alex <hirbu.alexandru@gmail.com>
2023-01-16 23:16:55 +0200

    few loops removed

--- game_while_loop.py
+++ game_while_loop.py
@@ -2,17 +2,23 @@ user_input = input('Press - help -  to start the program \n> ')
 while user_input != 'help' and user_input != 'HELP':
     print ('Incorect input, please try again')
     user_input = input('Press - help -  to start the program \n> ')
-else:
-    print ('start - to start the car \nstop - to stop the car \nquit - to exit')
-game_options = input('> ')    
-while game_options == 'start':
-    print ('Car started... Ready to go!')
 
-while game_options == 'stop':
-    print ('Car stopped.')
-    
-while game_options == 'quit':
-    break
+while True:
+
+    print ('start - to start the car \nstop - to stop the car \nquit - to exit')
+    game_options = input('> ')    
+
+    if game_options == 'start':
+        print ('Car started... Ready to go!')
+
+    elif game_options == 'stop':
+        print ('Car stopped.')
+        
+    elif game_options == 'quit':
+        exit()
+
+    else:
+        print('invalid')
 
 
 
