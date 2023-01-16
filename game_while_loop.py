b1002b0513e94e08b3921dfea11feacacd4e83a6
Alex <hirbu.alexandru@gmail.com>
2023-01-16 23:01:40 +0200

    added more loops

--- game_while_loop.py
+++ game_while_loop.py
@@ -4,6 +4,18 @@ while user_input != 'help' and user_input != 'HELP':
     user_input = input('Press - help -  to start the program \n> ')
 else:
     print ('start - to start the car \nstop - to stop the car \nquit - to exit')
+game_options = input('> ')    
+while game_options == 'start':
+    print ('Car started... Ready to go!')
+
+while game_options == 'stop':
+    print ('Car stopped.')
+    
+while game_options == 'quit':
+    break
+
+
+
 
 
 
