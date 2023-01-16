9cb7dcb6dacba5843cabfaf264b1590fb62697fb
Alex <hirbu.alexandru@gmail.com>
2023-01-16 22:47:25 +0200

    game start

---
+++ game_while_loop.py
@@ -0,0 +1,9 @@
+user_input = input('Press - help -  to start the program \n> ')
+while user_input != 'help' and user_input != 'HELP':
+    print ('Incorect input, please try again')
+    user_input = input('Press - help -  to start the program \n> ')
+else:
+    print ('start \nstop \nquit ')
+
+
+
