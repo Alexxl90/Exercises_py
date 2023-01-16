0086c0c45ec57aa38c296e0b3474283218e1d6dd
Alex <hirbu.alexandru@gmail.com>
2023-01-16 22:48:55 +0200

    game controls defined

--- game_while_loop.py
+++ game_while_loop.py
@@ -3,7 +3,7 @@ while user_input != 'help' and user_input != 'HELP':
     print ('Incorect input, please try again')
     user_input = input('Press - help -  to start the program \n> ')
 else:
-    print ('start \nstop \nquit ')
+    print ('start - to start the car \nstop - to stop the car \nquit - to exit')
 
 
 
